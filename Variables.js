bc5a335382932cc2d91afc5af81f9b53035085b6
AbalVishnu <abalvishnu.97@gmail.com>
2025-04-07 13:19:27 +0530

    Add files via upload

---
+++ Variables.js
@@ -0,0 +1,29 @@
+var a = 10;
+var A = 20;
+console.log(A)
+let ab = 10;
+let AB = 30;
+console.log(ab)
+const ca = 50;
+console.log(ca);
+
+
+
+let petname = "astro";
+console.log(petname);
+console.log(typeof petname);
+
+let DOB = 180197;
+console.log(DOB);
+console.log(typeof DOB);
+
+let TF = true;
+console.log(TF);
+console.log(typeof TF);
+
+let abal;
+console.log(abal);
+console.log(typeof abal);
+
+
+
---
+++ control_statement.js
@@ -0,0 +1,15 @@
+let a = 1;
+while (a <= 10) {
+console.log(a);
+
+a++;
+
+
+}
+
+let b = 1;
+do {
+console.log(b);
+
+b++;
+}while (b <= 10)
